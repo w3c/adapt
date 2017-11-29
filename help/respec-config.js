7daba499696d251bd49023a9a4ef81cb6957d041
ruoxiran <397740156@qq.com>
2017-11-29 19:43:55 +0800

    set link for explainer.html

--- help/respec-config.js
+++ help/respec-config.js
@@ -30,7 +30,7 @@ var respecConfig = {
     //previousDiffURI: "",
 
     // if there a publicly available Editors Draft, this is the link
-    edDraftURI: "https://w3c.github.io/w3c/personalization-semantics/explainer/help/help.html",
+    edDraftURI: "https://w3c.github.io/w3c/personalization-semantics/help/index.html",
 
     // if this is a LCWD, uncomment and set the end of its review period
     // lcEnd: "2012-02-21",
