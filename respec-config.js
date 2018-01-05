3de847fd8ccda5a2a537df6f49926186c0ea76f7
michael-n-cooper <cooper@w3.org>
2018-01-05 15:18:17 -0500

    Explicitly set license to document license

--- respec-config.js
+++ respec-config.js
@@ -21,6 +21,7 @@ var respecConfig = {
     // if you wish the publication date to be other than today, set this
     //publishDate:  "2017-05-09",
     copyrightStart:  "2017",
+    license: "document",
 
     // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
     // and its maturity status
