ae9121f0717d5cd82c8a5ae028073a55af385715
ruoxiran <397740156@qq.com>
2018-02-05 16:42:56 +0800

    Change the short name of content

--- content/respec-config.js
+++ content/respec-config.js
@@ -15,7 +15,7 @@ var respecConfig = {
     //diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 
     // the specifications short name, as in http://www.w3.org/TR/short-name/
-    shortName:            "personalization-content-1.0",
+    shortName:            "personalization-semantics-content-1.0",
 
 
     // if you wish the publication date to be other than today, set this
