1a7b5bc868d3330a5d03aa1c6a39a8e0c3743c2c
ruoxiran <397740156@qq.com>
2018-02-05 17:40:22 +0800

    Change the short name of explainer

--- respec-config.js
+++ respec-config.js
@@ -15,7 +15,7 @@ var respecConfig = {
     //diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 
     // the specifications short name, as in http://www.w3.org/TR/short-name/
-    shortName:            "personalization-explainer-1.0",
+    shortName:            "personalization-semantics-1.0",
 
 
     // if you wish the publication date to be other than today, set this
